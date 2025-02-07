c53e18ed524f19f2617002a834086f4853f12cad
Alexander Zuev <azuev@outlook.com>
2025-02-07 12:28:35 +0800

    refactor(errors): improve error handling in API endpoints and global exception handler

--- src/api/handlers/error_handlers.py
+++ src/api/handlers/error_handlers.py
@@ -1,6 +1,7 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
 
+from src.api.v0.schemas.base_schemas import ErrorCode, ErrorResponse
 from src.core._exceptions import NonRetryableError
 from src.infra.logger import get_logger
 
@@ -11,16 +12,20 @@ async def global_exception_handler(request: Request, exc: Exception) -> JSONResp
     """Global exception handler for any unhandled exceptions."""
     logger.critical(f"Unhandled exception at {request.url.path}: {exc}", exc_info=True)
     return JSONResponse(
-        status_code=500, content={"detail": "An internal server error occurred, please contact support."}
+        status_code=500,
+        content=ErrorResponse(
+            code=ErrorCode.SERVER_ERROR, detail="An internal server error occurred, please contact support."
+        ),
     )
 
 
-async def non_retryable_exception_handler(request: Request, exc: Exception) -> JSONResponse:
+async def non_retryable_exception_handler(request: Request, exc: NonRetryableError) -> JSONResponse:
     """Catch and log a non-retryable error."""
-    if isinstance(exc, NonRetryableError):
-        logger.error(f"Non-retryable error at {request.url.path}: {exc.error_message}")
-        return JSONResponse(
-            status_code=500,
-            content={"detail": f"An internal error occurred while processing your request: {exc.error_message}."},
-        )
-    return None
+    logger.error(f"Non-retryable error at {request.url.path}: {exc.error_message}")
+    return JSONResponse(
+        status_code=500,
+        content=ErrorResponse(
+            code=ErrorCode.SERVER_ERROR,
+            detail=f"An internal error occurred while processing your request: {exc.error_message}.",
+        ),
+    )
